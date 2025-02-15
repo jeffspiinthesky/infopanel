72ca53e6502ec65a6f142bc14179d2980f2be03b
Jeffs PI in the Sky <jeffs_piinthesky@gmail.com>
2025-02-15 14:04:55 +0000

    Changed to start matchbox window manager, flask app and epiphany

--- infopanel/usr/local/bin/kiosk.sh
+++ infopanel/usr/local/bin/kiosk.sh
@@ -3,9 +3,21 @@
 xset s noblank
 xset s off
 xset -dpms
-unclutter -idle 0.5 -root &
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+xrandr -o right
 
-python /usr/local/bin/switch.py &
-chromium-browser --noerrdialogs --enable-logging --disable-infobars --kiosk http://localhost | logger &
+cd /home/pi/infopanel/flask
+/home/pi/infopanel/flask/venv/bin/python infopanel.py 2>&1 | logger &
+CHECK=""
+while [ "${CHECK}" != "0" ]
+do
+	echo "Waiting for application to become available..." | logger
+	sleep 1
+	wget --spider http://localhost:5000
+	CHECK=$?
+done
+
+matchbox-window-manager -use_titlebar no &
+
+#chromium --display=:0 --kiosk --incognito --window-position=0,0 http://localhost:5000
+epiphany-browser --profile /home/pi/epiphany-profiles http://localhost:5000
+#epiphany-browser http://localhost:5000
